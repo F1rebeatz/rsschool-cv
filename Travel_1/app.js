0c730eda87efb95c3f14c15ae768541252452b7e
Alex <ahzeealex@gmail.com>
2022-07-13 20:02:16 +0300

    init: project

---
+++ Travel_1/app.js
@@ -0,0 +1,29 @@
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const menu = document.querySelector("#menu").cloneNode(1);
+
+hamb.addEventListener("click", hambHandler);
+
+function hambHandler(e) {
+  e.preventDefault();
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  renderPopup();
+}
+
+function renderPopup() {
+  popup.appendChild(menu);
+}
+
+const links = Array.from(menu.children);
+
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+
+function closeOnClick() {
+  popup.classList.remove("open");
+  hamb.classList.remove("active");
+}
+
+const image = document.createElement("img");
