2601f875c2397482eb691d17f37fe033d5013789
Alex <ahzeealex@gmail.com>
2022-07-25 22:15:47 +0300

    feat:popup

--- Travel_1/app.js
+++ Travel_1/app.js
@@ -1,6 +1,8 @@
 const hamb = document.querySelector("#hamb");
 const popup = document.querySelector("#popup");
+const overlay = document.querySelector("#overlay");
 const menu = document.querySelector("#menu").cloneNode(1);
+const mobMenu = document.querySelector("#mobMenu").cloneNode(1);
 
 hamb.addEventListener("click", hambHandler);
 
@@ -8,11 +10,14 @@ function hambHandler(e) {
   e.preventDefault();
   popup.classList.toggle("open");
   hamb.classList.toggle("active");
+  overlay.classList.toggle("show");
+  mobMenu.classList.toggle("act");
   renderPopup();
 }
 
 function renderPopup() {
   popup.appendChild(menu);
+  menu.appendChild(mobMenu);
 }
 
 const links = Array.from(menu.children);
@@ -24,6 +29,8 @@ links.forEach((link) => {
 function closeOnClick() {
   popup.classList.remove("open");
   hamb.classList.remove("active");
+  overlay.classList.remove("show");
+  mobMenu.classList.remove("act");
 }
 
 const image = document.createElement("img");
